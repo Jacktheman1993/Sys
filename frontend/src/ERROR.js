7308b5b12e7233b369dd27a10b9280a2769d0316
Jacktheman1993 <32671071+Jacktheman1993@users.noreply.github.com>
2018-11-16 14:43:02 +0100

    Token

---
+++ frontend/src/ERROR.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Error404(){
+    return (
+        <div>
+            <h1>EROOR !!!</h1>
+            <h2>YOU DO NOT HAVE ACCECS</h2>
+        </div>
+    )
+}
--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -2,28 +2,35 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import './App.css';
 import facade from './apiFacade';
+import Error from './ERROR';
+import jwtDecode from '../node_modules/jwt-decode';
 
 class Nav extends Component {
   constructor(props) {
     super(props);
-    this.state = {info: [] }
-    
+    this.state = { info: [], roleToken: [] }
   }
   async componentDidMount() {
     try {
       const info = await facade.getInfo();
-      this.setState({ info:info.results });
+      this.setState({ info: info.results });
+
+      const tokenDE = jwtDecode(facade.getToken());
+      const roleToken = tokenDE.roles;
+      this.setState({ roleToken: roleToken })
+      console.log(roleToken);
+
     } catch (err) {
       console.log("STATE :" + err)
     }
-    
+
   }
   render() {
     return (
-      
+
       <Router>
         <div>
-          
+
           <ul className="header">
             <li>
               <NavLink exact to="/">Home</NavLink>
@@ -40,9 +47,9 @@ class Nav extends Component {
           </ul>
 
           <Route path="/" component={Home} />
-          <Route path="/user" component={User} />
-          <Route path="/admin" component={Admin} />
-          <Route path="/info" render={ () => <InfoStar info={this.state.info}/>} />
+          <Route path="/user" render={() => <User roleToken={this.state.roleToken} />} />
+          <Route path="/admin" render={() => <Admin roleToken={this.state.roleToken} />} />
+          <Route path="/info" render={() => <Info info={this.state.info} />} />
         </div>
       </Router>
     )
@@ -51,41 +58,62 @@ class Nav extends Component {
 function Home() {
   return (
     <div>
-    
-    </div>
-  );
-}
-function User() {
-  return (
-    <div>
 
     </div>
   );
 }
-function Admin() {
-  return (
-    <div>
-
-    </div>
-  );
-}
-function InfoStar(props) {
-  const { info } = props;
-console.log("hej" , info)
-const rows = info.map(function(data){return <tr key = {data.url}><td>{data.name}</td><td>{data.height}</td><td>{data.gender}</td><td>{data.birth_year}</td></tr>})
-  
-return (
+function User(props) {
+  const roleToken = props;
+  console.log("ROLE ", roleToken);
+  if ("user" === roleToken) {
+    return (
       <div>
-        <h3>Info om Star Wars Personer</h3>
-        <table className="table">
+        <h2>User</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Error />
+      </div>
+    );
+  }
+
+}
+function Admin(props) {
+  const roleToken = props;
+  if ("admin" === roleToken) {
+    return (
+      <div>
+        <h2>Admin</h2>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <Error />
+      </div>
+    )
+  }
+
+}
+function Info(props) {
+  const { info } = props;
+  console.log("hej", info)
+  const rows = info.map(function (data) { return <tr key={data.url}><td>{data.name}</td><td>{data.height}</td><td>{data.gender}</td><td>{data.birth_year}</td></tr> })
+
+  return (
+    <div>
+      <h3>Info om Star Wars Personer</h3>
+      <table className="table">
         <thead>
           <tr><th>Name</th><th>Height</th><th>Gender</th><th>Birth-Year</th></tr>
         </thead>
         <tbody>
-        {rows}
+          {rows}
         </tbody>
       </table>
-      </div>
-    )
+    </div>
+  )
 }
 export default Nav;
