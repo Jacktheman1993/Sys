00713d5467b4895d13d0741b9d4c3e29e1c842f6
EFGottsche <32164956+EFGottsche@users.noreply.github.com>
2018-11-28 14:10:21 +0100

    Update for results from monday

--- frontend/src/results.js
+++ frontend/src/results.js
@@ -3,19 +3,17 @@ export default class Results extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            searchQuery: this.props.location.state.searchQuery
         }
     }
-    //     function expandResult(props, index) {
-    //     return (
-
-    //         );
-    // }
 
     render() {
+
         console.log(this.props);
         return (
             <div className='resultListContainer'>
+                Country: {this.props.location.state.searchQuery.country}, City: {this.props.location.state.searchQuery.city}, Date From: {this.props.location.state.searchQuery.dateF}, Date To: {this.props.location.state.searchQuery.dateT}
+
                 <img style={{height: 100, width: 100}} src="img/1.jpg" />
                 <ResultList
                     resultList={this.props.location.state.results}
@@ -25,56 +23,49 @@ export default class Results extends Component {
     }
 }
 
-const ResultList = ({ resultList }) =>
-    <div className="resultList">
-        {resultList.map(result => <div className="row" key={result.id}>
+const ResultList = ({ resultList }) => <div className="resultList">
+        {resultList.map(result => {
+            let rooms = result.rooms.map((room) => <tr className="room" key={room.id}>
+                <td>
+                    {room.name}
+                </td>
+                <td>
+                    {room.description}
+                </td>
+                <td>
+                    {room.bed}
+                </td>
+                <td>
+                    {room.price}
+                </td>
+                <td><form>
+                    <button>Book room</button>
+                </form></td>
+            </tr>);
+            return (
+                <div className="row" key={result.id}>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            {result.name}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{result.hotelDescription}</td>
-                        <td><form >
-                            <button>See available rooms</button>
-                        </form></td>
-                    </tr>
-                </tbody>
-                {/* <tbody>
-
-                    {<RoomList
-                        roomList={[{room: result.rooms}]}
-                    }
-                </tbody> */}
-
-            </table>
-        </div>)}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    {result.name}
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{result.hotelDescription}</td>
+                                <td><form >
+                                    <button>See available rooms</button>
+                                </form></td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            {rooms}
+                        </tbody>
+                    </table>
+                </div>)
+        }
+        )}
     </div>
-
-// const RoomList = ({ roomList }) => {
-//     {
-//         roomList.map(room => <tr className="room" key={room.id}>
-//             <td>
-//                 {room.name}
-//             </td>
-//             <td>
-//                 {room.description}
-//             </td>
-//             <td>
-//                 {room.bed}
-//             </td>
-//             <td>
-//                 {room.price}
-//             </td>
-//             <td><form>
-//                 <button>Book room</button>
-//             </form></td>
-//         </tr>)
-//     }
-// }
-//onSubmit={this.expandResult}
