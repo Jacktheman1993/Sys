e9ab32254e82b1d1250310242843734eb8fabdc7
tiba666 <519338+tiba666@users.noreply.github.com>
2018-12-04 10:49:02 +0100

    Cleaned up

--- backend/src/main/java/rest/HotelAPIResource.java
+++ backend/src/main/java/rest/HotelAPIResource.java
@@ -62,41 +62,4 @@ public class HotelAPIResource {
         List<DTORoom> dtos = fc.getRooms(id);
         return Response.ok(gson.toJson(dtos)).build();
     }
-
-    /**
-     * Returns all hotels matching the query params in the database, complete
-     * with rooms attached.
-     *
-     * @param country The name of the country to look for
-     * @param city The name of the city to look for
-     * @param dateF The date from which the user wants to look for
-     * @param dateT The date to which the user wants to look for
-     * @return a json string with the hotel info
-     */
-    @GET
-    @Path("search")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getHotelsSearch(@QueryParam("country") String country, @QueryParam("city") String city) {
-
-        return Response.ok(gson.toJson(fc.getHotelsSearch(country, city))).build();
-
-    }
-
-    /**
-     * THIS IS ONLY MEANT FOR IF WE CHANGE THE STRUCTURE OF THE API TO SEND A
-     * SMALLER AMOUNT OF INFO TO THE FRONTEND TO SHOW IN THE RESULT LIST AND
-     * THEN MAKE AN ADDITIONAL CALL WHEN LOOKING AT DETAILS ABOUT THE HOTEL
-     * SELECTED
-     *
-     * Returns a specific hotel in the database, complete with rooms attached.
-     *
-     * @return a json string with the hotel info
-     */
-//    @GET
-//    @Path("specific/{id}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Response getHotelByID(@PathParam("id") int id) {
-//        //return Response.ok(gson.toJson(fc.getSpecificHotel())).build();
-//        throw new UnsupportedOperationException("Not yet implemented");
-//    }
 }
