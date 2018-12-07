75212fa71dabf1a20cf4438f5cab710bff3cf1f6
unknown <esben.gottsche@gmail.com>
2018-12-07 10:42:45 +0100

    Made search fields required

--- frontend/src/search.js
+++ frontend/src/search.js
@@ -47,21 +47,21 @@ export default class Search extends Component {
                 <form onSubmit={this.search}>
                     <div className='row'>
                         <div className='col'>
-                            <input className='form-control' placeholder="Country" id="country" name="country" value={this.state.newSearch.country} onChange={this.inputVal} />
+                            <input required className='form-control' placeholder="Country" id="country" name="country" value={this.state.newSearch.country} onChange={this.inputVal} />
                         </div>
                         <div className='col'>
-                            <input className='form-control' placeholder="City" id="city" name="city" value={this.state.newSearch.city} onChange={this.inputVal} />
+                            <input required className='form-control' placeholder="City" id="city" name="city" value={this.state.newSearch.city} onChange={this.inputVal} />
                         </div>
                         <h2>Dato From</h2>
                         <div className='col'>
-                            <input className='form-control' type="date" placeholder="Dato To" id="datoF" name="datoF" value={this.state.newSearch.datoF} onChange={this.inputVal} />
+                            <input required className='form-control' type="date" placeholder="Dato To" id="datoF" name="datoF" value={this.state.newSearch.datoF} onChange={this.inputVal} />
                         </div>
                         <h2>Dato To</h2>
                         <div className='col'>
-                            <input className='form-control' type="date" placeholder="Dato From" id="datoT" name="datoT" value={this.state.newSearch.datoT} onChange={this.inputVal} />
+                            <input required className='form-control' type="date" placeholder="Dato From" id="datoT" name="datoT" value={this.state.newSearch.datoT} onChange={this.inputVal} />
                         </div>
                         <div className='col'>
-                            <button className='btn btn-lg btn-primary' >Søg</button>
+                            <button className='btn btn-lg btn-primary' >Search</button>
                         </div>
                     </div>
                 </form>
